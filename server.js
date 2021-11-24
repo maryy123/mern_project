78540428a032c7f5e7e912179bbcb88ac21c43b1
maryy123 <myriam.charfeddine@etudiant-fst.utm.tn>
2021-11-24 11:34:02 +0100

    thirdd version

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/api/config/paypal", (req, res) => {
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
   });
 }
 
