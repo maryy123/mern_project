9bd8d887cb29a04b35ee3bdd5cea8d20170d61c5
maryy123 <myriam.charfeddine@etudiant-fst.utm.tn>
2021-12-21 16:16:34 +0100

    new

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ if (process.env.NODE_ENV == "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+} else {
+  require("dotenv").config();
 }
 
 const PORT = process.env.PORT || 7777;
